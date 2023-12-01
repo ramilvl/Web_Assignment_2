dfe5e83424629375c50f8d57bc3b04da2818358b
Ramil Valiyev <ramil4360616@gmail.com>
2023-12-01 12:30:38 +0400

    search input is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,46 @@
+let data;
+
 fetch("https://dummyjson.com/products")
   .then((res) => res.json())
-
   .then((json) => {
     data = json.products;
+    displayProducts(data);
+  })
+  .catch((err) => console.error("Error happened while fetching data:", err));
 
-    const containerDiv = document.getElementsByClassName("container")[0];
+function displayProducts(products) {
+  const containerDiv = document.getElementsByClassName("container")[0];
+  containerDiv.innerHTML = "";
 
-    data.forEach((product) => {
+  products.forEach((product) => {
+    let thumbnailEl = product.images.filter((link) =>
+      link.includes("thumbnail")
+    );
+    thumbnailEl = thumbnailEl.length == 0 ? product.images[0] : thumbnailEl[0];
 
-      let thumbnailEl = product.images.filter((link) =>
-        link.includes("thumbnail")
-      );
-      thumbnailEl = thumbnailEl.length == 0 ? product.images[0] : thumbnailEl[0];
+    const productsDiv = document.createElement("div");
+    productsDiv.setAttribute("class", "products");
+    productsDiv.innerHTML = `
+                    <img src="${thumbnailEl}" alt="${product.title}" />
+                    <h2>${product.title}</h2>
+                    <p class="color-666">Price: ${product.price}</p>
+                    <p class="color-666">Discount: ${product.discountPercentage}%</p>
+                    <p class="color-666">Category: ${product.category}</p>
+                    <p class="color-666">Stock: ${product.stock}</p>
+                `;
 
-      const productsDiv = document.createElement("div");
-      productsDiv.setAttribute("class", "products")
+    containerDiv.appendChild(productsDiv);
+  });
+}
 
-      productsDiv.innerHTML = `
-                <img src="${thumbnailEl}" alt="${product.title}" />
-                <h2>${product.title}</h2>
-                <p class="color-666">Price: ${product.price}</p>
-                <p class="color-666">Discount: ${product.discountPercentage}%</p>
-                <p class="color-666">Category: ${product.category}</p>
-                <p class="color-666">Stock: ${product.stock}</p>
-            `;
+function search() {
+  const keyword = document.getElementById("searchInput").value.toLowerCase();
+  const filteredProducts = data.filter(
+    (product) =>
+      product.title.toLowerCase().includes(keyword) ||
+      product.description.toLowerCase().includes(keyword) ||
+      product.category.toLowerCase().includes(keyword)
+  );
 
-        containerDiv.appendChild(productsDiv);
-    });
-})
-
-.catch((err) => console.error("Error happened while fetching data:", err));
+  displayProducts(filteredProducts);
+}
